62642d4261fcb72fec44af9d820e8d86e926e881
Tien <ngotienpts@gmail.com>
2022-11-24 17:30:36 +0700

    24/11

---
+++ js/main.js
@@ -0,0 +1,139 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // back top
+  var backTop = document.querySelector("#back-top");
+
+  // width document
+  var widthDoc = document.querySelector("body");
+
+  const app = {
+    // su ly cac su kien
+    handleEvent: function () {
+      const _this = this;
+
+      // when click back top
+      if (backTop) {
+        backTop.onclick = function () {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        };
+      }
+
+      // emagazin
+      if (widthDoc) {
+        var expNoEditFull = document.querySelectorAll(".expNoEdit.full");
+        expNoEditFull.forEach(function (a) {
+          a.style.width = widthDoc.clientWidth + "px";
+          a.style.marginLeft = "-" + a.offsetLeft + "px";
+        });
+      }
+      // hide cac element khi click ra ngoai
+      document.addEventListener("click", function (e) {});
+    },
+
+    // slide topic list
+    slideToppicList: function () {
+      $(".topic-list").slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      });
+    },
+    // slide hightlight
+    slideHightlight: function () {
+      $(".hightlight-top__slide").slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      });
+    },
+    // scroll top
+    scrollFunc: function () {
+      if (backTop) {
+        if (
+          document.body.scrollTop > 300 ||
+          document.documentElement.scrollTop > 300
+        ) {
+          backTop.style.opacity = 1;
+          backTop.style.visibility = "visible";
+        } else {
+          backTop.style.opacity = 0;
+          backTop.style.visibility = "hidden";
+        }
+      }
+    },
+    // window scroll
+    windowScroll: function () {
+      var _this = this;
+      window.onscroll = function () {
+        // scroll top
+        _this.scrollFunc();
+      };
+    },
+    // khoi tao function start
+    start: function () {
+      // su ly cac su kien
+      this.handleEvent();
+      // window scroll
+      this.windowScroll();
+      // slide topic list
+      this.slideToppicList();
+      // slide hightlight
+      this.slideHightlight();
+    },
+  };
+
+  app.start();
+});
